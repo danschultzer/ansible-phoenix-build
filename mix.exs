857667d7226e8472c28a55e1d1eed34af8114dc6
Dan Schultzer <dan@dreamconception.com>
2017-12-03 11:23:35 -0800

    Build docker image

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule AnsiblePhoenixBuild.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+
+      {:distillery, "~> 1.5.2", only: [:dev, :test]}
     ]
   end
 
